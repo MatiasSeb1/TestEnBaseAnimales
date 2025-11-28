d36cc8858f444367a42e51d032b0c1212f3c5e80
Mat <macarenaesmibesto@gmail.com>
2025-11-27 22:19:40 -0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/TestEnBaseAnimales/',
   server: {
     port: 3000,
     open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
   },
 })
